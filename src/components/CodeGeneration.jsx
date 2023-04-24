b54c989b90a47f084bf3a4517d03681252619f07
Rustam_9724 <Rustam_9724@mail.ru>
2023-04-24 13:51:04 +0500

    init: CodeGenerate component functional

--- src/components/CodeGeneration.jsx
+++ src/components/CodeGeneration.jsx
@@ -3,14 +3,27 @@ import { useState } from 'react';
 function CodeGeneration() {
     const [firstTable, setFirstTable] = useState(1);
     const [lastTable, setLastTable] = useState(42);
+    const [selectedTable, setSelectedTable] = useState(null);
+    const [codeForm, setCodeForm] = useState('square');
 
     let tables = [];
     for (let i = firstTable; i <= lastTable; i++) {
+        let num = i;
         tables.push(
-            <div className="code__generation__main__left__choose-table__tables__table">
+            <div className={selectedTable === num ? 'selected' : null} key={i} onClick={event => setSelectedTable(num)}>
                 {i}
             </div>
         )
+    } 
+
+    function selectChangeHandler(event) {
+        if (event.target.value === 'Квадратный') {
+            setCodeForm('square')
+        } else if (event.target.value === 'Прямоугольный') {
+            setCodeForm('rectangle');
+        } else if (event.target.value === 'Круглый') {
+            setCodeForm('circle')
+        }
     } 
 
     return (
@@ -21,11 +34,11 @@ function CodeGeneration() {
                     <div className="code__generation__main__left__section">
                         <label>
                             <p>Номера столов от</p>
-                            <input type="number" placeholder="Выберите номер"/>
+                            <input type="number" placeholder="Выберите номер" value={firstTable < 1 ? 1 : firstTable} onChange={event => setFirstTable(event.target.value < 1 ? 1 : event.target.value)}/>
                         </label>
                         <label>
                             <p>Номера столов до</p>
-                            <input type="number" placeholder="Выберите номер"/>
+                            <input type="number" placeholder="Выберите номер" value={lastTable > 42 ? 42 : lastTable} onChange={event => setLastTable(event.target.value > 42 ? 42 : event.target.value)}/>
                         </label>
                     </div>
                     <div className="code__generation__main__left__section">
@@ -41,7 +54,7 @@ function CodeGeneration() {
                     <div className="code__generation__main__left__section">
                         <label>
                             <p>Вид QR-кода</p>
-                            <select>
+                            <select onChange={event => selectChangeHandler(event)}>
                                 <option>Квадратный</option>
                                 <option>Прямоугольный</option>
                                 <option>Круглый</option>
@@ -64,19 +77,19 @@ function CodeGeneration() {
                             <p>Круг</p>
                         </div>
                         <div className="code-generation__main__right__header__line">
-                            <div></div>
-                            <div></div>
-                            <div></div>
+                            <div className={codeForm === 'square' ? 'selected' : null}></div>
+                            <div className={codeForm === 'rectangle' ? 'selected' : null}></div>
+                            <div className={codeForm === 'circle' ? 'selected' : null}></div>
                         </div>
                     </div>
-                    <div className="code-generation__main__right__QR-section">
-                        <h5>Вкус<span>Клик</span></h5>
-                        <p className="code-generation__main__right__QR-section__view-menu">Посмотреть меню</p>
-                        <p className="code-generation__main__right__QR-section__description">Lemon drops chocolate cake gummies carrot</p>
-                        <div className="code-generation__main__right__QR-section__QR-code"></div>
+                    <div className={`code-generation__main__right__QR-section ${codeForm}`}>
+                        <h5 className={codeForm}>Вкус<span>Клик</span></h5>
+                        <p className={`code-generation__main__right__QR-section__view-menu ${codeForm}`}>Посмотреть меню</p>
+                        <p className={`code-generation__main__right__QR-section__description ${codeForm}`}>Lemon drops chocolate cake gummies carrot</p>
+                        <div className={`code-generation__main__right__QR-section__QR-code ${codeForm}`}></div>
                         <a href="!#">vkusclick.ru/yFnEq/13</a>
                         <p className="code-generation__main__right__QR-section__description">Номер вашего стола</p>
-                        <div className="code-generation__main__right__QR-section__table-number">13</div>
+                        <div className={`code-generation__main__right__QR-section__table-number ${codeForm}`}>{selectedTable}</div>
                     </div>
                     <div className="code-generation__main__right__download-section">
                         <select>
