f6ebe4993b68e8013f4fcbea5b1684c46430f04c
Rustam_9724 <Rustam_9724@mail.ru>
2023-05-05 16:34:03 +0500

    init: CubCalc style

--- src/components/SubCalc.jsx
+++ src/components/SubCalc.jsx
@@ -1,14 +1,20 @@
 function SubCalc() {
+    
+    function moduleClickHandler(event) {
+        let target = event.target.closest("div.sub-calc__main__module");
+        target.childNodes[0].childNodes[0].checked = !target.childNodes[0].childNodes[0].checked;
+    }
+    
     return (
         <div className="sub-calc">
             <div className="sub-calc__header">
-                <h2>Калькулято подписки</h2>
+                <h2>Калькулятор подписки</h2>
                 <div>
                     <div></div>
                     <p>Пополнить баланс</p>
                 </div>
             </div>
-            <div className="sub-calc__main">
+            <div className="sub-calc__main" onClick={moduleClickHandler}>
                 <div className="sub-calc__main__module">
                     <div className="sub-calc__main__module__checkbox">
                         <input type="checkbox" id="first" hidden/>
@@ -24,7 +30,7 @@ function SubCalc() {
                 <div className="sub-calc__main__module __disabled">
                     <h4>Название модуля</h4>
                     <p>Если Вы не нашли вопрос в нашем FAQ, Вы можете связаться с нами. Мы ответим Вам в ближайшее время! Если Вы не нашли вопрос в нашем FAQ, Вы можете связаться с нами. Мы ответим Вам в ближайшее время!</p>
-                    <div>
+                    <div className="sub-calc__main__module__price">
                         <p>В разработке</p>
                     </div>
                 </div>
@@ -35,7 +41,7 @@ function SubCalc() {
                     </div>
                     <h4>Название модуля</h4>
                     <p>Если Вы не нашли вопрос в нашем FAQ, Вы можете связаться с нами. Мы ответим Вам в ближайшее время! Если Вы не нашли вопрос в нашем FAQ, Вы можете связаться с нами. Мы ответим Вам в ближайшее время!</p>
-                    <div>
+                    <div className="sub-calc__main__module__price">
                         <p>29 руб./день</p>
                         <p>829 руб./месяц</p>
                     </div>
@@ -47,19 +53,19 @@ function SubCalc() {
                     </div>
                     <h4>Название модуля</h4>
                     <p>Если Вы не нашли вопрос в нашем FAQ, Вы можете связаться с нами. Мы ответим Вам в ближайшее время! Если Вы не нашли вопрос в нашем FAQ, Вы можете связаться с нами. Мы ответим Вам в ближайшее время!</p>
-                    <div>
+                    <div className="sub-calc__main__module__price">
                         <p>29 руб./день</p>
                         <p>829 руб./месяц</p>
                     </div>
                 </div>
                 <div className="sub-calc__main__module">
                     <div className="sub-calc__main__module__checkbox">
-                        <input type="checkbox" id="fiаth" hidden/>
+                        <input type="checkbox" id="fifth" hidden/>
                         <label htmlFor="fifth"></label>
                     </div>
                     <h4>Название модуля</h4>
                     <p>Если Вы не нашли вопрос в нашем FAQ, Вы можете связаться с нами. Мы ответим Вам в ближайшее время! Если Вы не нашли вопрос в нашем FAQ, Вы можете связаться с нами. Мы ответим Вам в ближайшее время!</p>
-                    <div>
+                    <div className="sub-calc__main__module__price">
                         <p>29 руб./день</p>
                         <p>829 руб./месяц</p>
                     </div>
@@ -71,15 +77,15 @@ function SubCalc() {
                     </div>
                     <h4>Название модуля</h4>
                     <p>Если Вы не нашли вопрос в нашем FAQ, Вы можете связаться с нами. Мы ответим Вам в ближайшее время! Если Вы не нашли вопрос в нашем FAQ, Вы можете связаться с нами. Мы ответим Вам в ближайшее время!</p>
-                    <div>
+                    <div className="sub-calc__main__module__price">
                         <p>29 руб./день</p>
                         <p>829 руб./месяц</p>
                     </div>
                 </div>
             </div>
             <div className="sub-calc__consumtion">
-                <p>Текущий расход: <b>98 руб./день</b> или <b>980 руб./месяц</b></p>
-                <p>Прогноз расхода: <b>98 руб./день</b> или <b>980 руб./месяц</b></p>
+                <div><pre>Текущий расход: </pre> <pre>98 руб./день </pre> <pre>или </pre> <pre>980 руб./месяц </pre></div>
+                <div><pre>Прогноз расхода: </pre> <pre>98 руб./день </pre> <pre>или </pre> <pre>980 руб./месяц </pre></div>
             </div>
             <button>Применить</button>
         </div>
