849565e820cbc37521df392099d23e540d0231d2
Rustam_9724 <Rustam_9724@mail.ru>
2023-05-19 09:54:13 +0500

    init: SubCalc component functional

--- src/components/SubCalc.jsx
+++ src/components/SubCalc.jsx
@@ -1,12 +1,67 @@
+import { useState } from 'react';
+
 function SubCalc() {
-    
+    const [isPopup, setIsPopup] = useState(false);
+    const [service, setService] = useState('');
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
+    const [isServiceValid, setIsServiceValid] = useState(true);
+    const [isNameValid, setIsNameValid] = useState(true);
+    const [isNumberValid, setIsNumberValid] = useState(true);
+
     function moduleClickHandler(event) {
         let target = event.target.closest("div.sub-calc__main__module");
         target.childNodes[0].childNodes[0].checked = !target.childNodes[0].childNodes[0].checked;
+        target.classList.toggle('checked');
+    }
+
+    function sendingApp() {
+       setIsServiceValid(true);
+       setIsNameValid(true);
+       setIsNumberValid(true);
+
+       if (!service) {
+           setIsServiceValid(false);
+       }
+
+       if (!name) {
+           setIsNameValid(false);
+       }
+
+       if (!number) {
+           setIsNumberValid(false)
+       }
+
+       if (service && name && number) {
+           setService('');
+           setName('');
+           setNumber('');
+           setIsServiceValid(true);
+           setIsNameValid(true);
+           setIsNumberValid(true);
+           setIsPopup(false);
+       }
     }
     
     return (
         <div className="sub-calc">
+            <div className={`sub-calc__substrate ${isPopup ? 'active' : null}`} onClick={() => setIsPopup(false)}></div>
+            <div className={`sub-calc__popup ${isPopup ? 'active' : null}`}>
+                <h4>Заказ услуг</h4>
+                <label>
+                    <p>Услуги{!isServiceValid && <span>*</span>}</p>
+                    <input type="text" placeholder="Введите услугу" value={service} onChange={event => setService(event.target.value)} className={!isServiceValid ? 'invalid' : null}/>
+                </label>
+                <label>
+                    <p>Имя{!isNameValid && <span>*</span>}</p>
+                    <input type="text" placeholder="Введите имя" value={name} onChange={event => setName(event.target.value)} className={!isNameValid ? 'invalid' : null}/>
+                </label>
+                <label>
+                    <p>Телефон{!isNameValid && <span>*</span>}</p>
+                    <input type="text" placeholder="Введите телефон" value={number} onChange={event => setNumber(event.target.value)} className={!isNumberValid ? 'invalid' : null}/>
+                </label>
+                <button onClick={sendingApp}>Отправить заявку</button>
+            </div>
             <div className="sub-calc__header">
                 <h2>Калькулятор подписки</h2>
                 <div>
@@ -14,8 +69,8 @@ function SubCalc() {
                     <p>Пополнить баланс</p>
                 </div>
             </div>
-            <div className="sub-calc__main" onClick={moduleClickHandler}>
-                <div className="sub-calc__main__module">
+            <div className="sub-calc__main">
+                <div className="sub-calc__main__module" onClick={event => moduleClickHandler(event)}>
                     <div className="sub-calc__main__module__checkbox">
                         <input type="checkbox" id="first" hidden/>
                         <label htmlFor="first"></label>
@@ -27,15 +82,15 @@ function SubCalc() {
                         <p>829 руб./месяц</p>
                     </div>
                 </div>
-                <div className="sub-calc__main__module __disabled">
+                <div className="sub-calc__main__module disabled">
                     <h4>Название модуля</h4>
                     <p>Если Вы не нашли вопрос в нашем FAQ, Вы можете связаться с нами. Мы ответим Вам в ближайшее время! Если Вы не нашли вопрос в нашем FAQ, Вы можете связаться с нами. Мы ответим Вам в ближайшее время!</p>
                     <div className="sub-calc__main__module__price">
                         <p>В разработке</p>
                     </div>
                 </div>
-                <div className="sub-calc__main__module">
-                <div className="sub-calc__main__module__checkbox">
+                <div className="sub-calc__main__module" onClick={event => moduleClickHandler(event)}>
+                    <div className="sub-calc__main__module__checkbox">
                         <input type="checkbox" id="third" hidden/>
                         <label htmlFor="third"></label>
                     </div>
@@ -46,7 +101,7 @@ function SubCalc() {
                         <p>829 руб./месяц</p>
                     </div>
                 </div>
-                <div className="sub-calc__main__module">
+                <div className="sub-calc__main__module" onClick={event => moduleClickHandler(event)}>
                     <div className="sub-calc__main__module__checkbox">
                         <input type="checkbox" id="fourth" hidden/>
                         <label htmlFor="fourth"></label>
@@ -58,7 +113,7 @@ function SubCalc() {
                         <p>829 руб./месяц</p>
                     </div>
                 </div>
-                <div className="sub-calc__main__module">
+                <div className="sub-calc__main__module" onClick={event => moduleClickHandler(event)}>
                     <div className="sub-calc__main__module__checkbox">
                         <input type="checkbox" id="fifth" hidden/>
                         <label htmlFor="fifth"></label>
@@ -70,7 +125,7 @@ function SubCalc() {
                         <p>829 руб./месяц</p>
                     </div>
                 </div>
-                <div className="sub-calc__main__module">
+                <div className="sub-calc__main__module" onClick={event => moduleClickHandler(event)}>
                     <div className="sub-calc__main__module__checkbox">
                         <input type="checkbox" id="sixth" hidden/>
                         <label htmlFor="sixth"></label>
@@ -87,7 +142,7 @@ function SubCalc() {
                 <div><pre>Текущий расход: </pre> <pre>98 руб./день </pre> <pre>или </pre> <pre>980 руб./месяц </pre></div>
                 <div><pre>Прогноз расхода: </pre> <pre>98 руб./день </pre> <pre>или </pre> <pre>980 руб./месяц </pre></div>
             </div>
-            <button>Применить</button>
+            <button onClick={() => setIsPopup(true)}>Применить</button>
         </div>
     )
 }
