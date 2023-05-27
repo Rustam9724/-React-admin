dc1088971242fcb1915785facf9f3cb6f323f4a3
rustam_9724 <rustam_9724@mail.ru>
2023-05-27 14:53:22 +0500

    fix: return function to the Login component in the ResetPssword component

--- src/components/ResetPassword.jsx
+++ src/components/ResetPassword.jsx
@@ -1,20 +1,43 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 function ResetPassword() {
+    const [email, setEmail] = useState('');
+    const [isEmailValid, setIsEmailValid] = useState(true);
+    const navigate = useNavigate();
+
+    function codeSend() {
+        setIsEmailValid(false);
+
+        if (!isEmailValid) {
+            setIsEmailValid(false);
+        } else {
+            navigate('/')
+        }
+    }
+
     return (
         <div className="reset-password-section">
             <div className="reset-password__top-shape"></div>
              <div className="reset-password__form">
                  <div className="reset-password__steps">
-                     <div className="steps__first-step"></div>
-                     <div className="steps__arrow"></div>
-                     <div className="steps__second-step"></div>
-                     <div className="steps__arrow"></div>
-                     <div className="steps__third-step"></div>
+                    <div className="steps__first-step"></div>
+                    <div className="steps__arrow"></div>
+                    <div className="steps__second-step"></div>
+                    <div className="steps__arrow"></div>
+                    <div className="steps__third-step"></div>
                  </div>
                  <label>
-                    <p>Email</p>
-                    <input type="text" placeholder="Placeholder"/>
+                    <p>Email {!isEmailValid && <span>*</span>}</p>
+                    <input 
+                        type="text" 
+                        placeholder="Placeholder" 
+                        value={email} 
+                        onChange={event => setEmail(event.target.value)} 
+                        className={!isEmailValid ? 'invalid' : null}
+                    />
                  </label>
-                 <button>Отправить код</button>
+                 <button onClick={codeSend}>Отправить код</button>
             </div>
             <div className="reset-password__bottom-shape"></div>  
         </div>
